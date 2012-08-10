41c638b6ec88f19f09b1d06d68623a9af038021c
Tim Gittos <timg@spiceworks.com>
2012-08-10 17:20:53 -0500

    Fixed bugs in recent changes, improved filename generation

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,1 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+APP_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/config.yml")
--- lib/github.rb
+++ lib/github.rb
@@ -14,7 +14,7 @@ module Github
       url = commit["url"]
       doc = Nokogiri::HTML(open(url))
       # extract the diff for this commit
-      commit["desc"] = doc.css(".commit-desc pre").first.content.gsub(/\n/, ' ').squeeze(' ')
+      commit["desc"] = doc.css(".commit-desc pre").first.content.gsub(/\n/, ' ').squeeze(' ') unless doc.css('.commit-desc pre').first.nil?
       doc.css('#files .file').each do |diff|
         commit["diffs"] << diff.to_s
       end
@@ -27,56 +27,60 @@ module Github
   def self.write_and_publish_post(filename, content, repo)
     @blog_path = APP_CONFIG['blog']['path']
     ensure_category_for_repo repo
-    post_location = File.join('.', "#{@blog_path}/#{repo}/_posts")
-    open(File.join(post_location, filename), 'w') do |f|
+    post_location = File.join("#{@blog_path}/#{repo}/_posts")
+    File.open(File.join(post_location, filename), 'w') do |f|
       f.puts content
     end
-    `jekyll #{APP_CONFIG['blog']['jekyll']['root']} #{APP_CONFIG['blog']['jekyll']['output']}`
-    `(cd #{APP_CONFIG['blog']['jekyll']['root']} && git add -A && git commit -m 'Gitblogger added some commits' && git push)`
+    #`jekyll #{APP_CONFIG['blog']['jekyll']['root']} #{APP_CONFIG['blog']['jekyll']['output']}`
+    #`(cd #{APP_CONFIG['blog']['jekyll']['root']} && git add -A && git commit -m 'Gitblogger added some commits' && git push)`
   end
 
   def self.ensure_category_for_repo(repo)
     write_repo_dir repo unless File.exists? "#{@blog_path}/#{repo}"
     write_repo_index repo unless File.exists? "#{@blog_path}/#{repo}/index.textile"
-    write_repo_post repo unless Dir.entries("#{@blog_path}/_posts").select{|e| e =~ /#{repo}/}
+    write_repo_post repo unless Dir.entries("#{@blog_path}/_posts").select{|e| e =~ /#{repo}/}.length > 0
   end
 
   private
 
   def self.write_repo_dir(repo)
+    Rails.logger.info "Creating directory for #{repo} at #{@blog_path}/#{repo}"
+    Rails.logger.info "Creating posts directory for #{repo} at #{@blog_path}/#{repo}/_posts"
     Dir.mkdir("#{@blog_path}/#{repo}")
     Dir.mkdir("#{@blog_path}/#{repo}/_posts")
   end
 
   def self.write_repo_index(repo)
+    Rails.logger.info "Creating index for new repo at #{@blog_path}/#{repo}/index.textile"
     File.open("#{@blog_path}/#{repo}/index.textile", 'w') do |f|
       f.write <<-EOS
-      ---
-      layout: page
-      parent: true
-      category: #{repo}
-      tags: github commit code
-      ---
+---
+layout: page
+parent: true
+category: #{repo}
+tags: github commit code
+---
 
-      <div id="post-list">
-        {% include post-list.html %}
-      </div>
+<div id="post-list">
+  {% include post-list.html %}
+</div>
       EOS
     end
   end
 
   def self.write_repo_post(repo)
-    File.open("#{@blog_path}/_posts/#{Date.today.strftime("%Y-%m-%d")}-#{repo}.textile", 'w') do |f|
+    Rails.logger.info "Creating commit post for new repo at #{@blog_path}/_posts"
+    File.open("#{@blog_path}/_posts/#{Date.today.strftime("%Y-%m-%d")}-#{repo}.textile".squeeze('-'), 'w') do |f|
       f.write <<-EOS
-      ---
-      layout: page
-      parent: true
-      category: #{repo}
-      tags: github commit code
-      ---
+---
+layout: page
+parent: true
+category: #{repo}
+tags: github commit code
+---
 
-      THIS IS A DUMMY ENTRY
-      EOS
+THIS IS A DUMMY ENTRY
+EOS
     end
   end
 
